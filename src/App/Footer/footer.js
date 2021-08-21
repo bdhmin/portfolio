2eb9c0df18633ab50abc0ea7223850f582b298d3
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2021-08-21 01:26:05 -0700

    Updated Korean text

--- src/App/Footer/footer.js
+++ src/App/Footer/footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="Footer">
       <Socials />
-      <span className="msg">정성스럽게 만든</span>
+      <span className="msg">정성으로 만든</span>
       <span className="cp">Copyright © 2021 – Bryan Min</span>
     </div>
   )
