5c1a0ba614aadd46858d8d893eec8ed1cd8c2b21
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2021-08-22 13:28:34 -0700

    Updated About section pt2

--- src/App/About/about.js
+++ src/App/About/about.js
@@ -17,17 +17,18 @@ function About() {
 
           <div className="Bio">
             <p>
-              I’m a rising third-year student at <a class="ucsd" href="https://ucsd.edu" target="_blank" rel="noreferrer">UC San Diego</a> studying for a B.S. in Computer Science.
+              I’m a rising third-year student at <a class="ucsd" href="https://ucsd.edu" target="_blank" rel="noreferrer">UC San Diego</a> studying for a B.S. in Computer Science. 
+              I'm a developer who focuses on building tools for self-improvement. 
+              I truly believe that everyone can become a better version of themselves with the right motivators and tools, and hopefully my projects can make a difference in my users' learning experience.
+              I also build other things for fun that are related to my hobbies such as tennis, <a class="glry" href="/gallery">photography</a>, and video games.
             </p>
             {/* <p>
               I began my creative journey with a curiosity for what aesthetics means to me. Along the way, I became inspired by the minimalist era, and I now strive to produce work that is clean yet deep in emotion.
             </p> */}
             <p>
               {/* I'm a developer who loves to build tools that improve the traits of the user using it—traits like productivity, knowledge in a particular field, or even tennis.  */}
-              I'm a developer who loves to build tools for self-improvement in productivity, knowledge in a particular field, or even gaming.
-            </p>
-            <p>
-              Outside of development, I play tennis, take <a class="glry" href="/gallery">photos</a>, and occasionally play some video games.
+              I grew up in Seoul, South Korea, and then moved to the Bay Area for high school. I've been roaming around California ever since. 
+              Though it's great living in California (amazing weather), I do sometimes miss Korea's food and Seoul's entire vibe. That's okay, I'll go visit very soon!
             </p>
             {/* <p>
               Scroll up to see my <b>live status</b>! If you want to learn how I did this, <Link to="/docs/live-status"><u>click here</u></Link>.
