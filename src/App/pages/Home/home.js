20041a0e4116cf0f4cf00726e0b49ab62aefedf2
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-11-03 18:31:15 -0700

    updated home

--- src/App/pages/Home/home.js
+++ src/App/pages/Home/home.js
@@ -58,7 +58,7 @@ class Home extends Component {
                     <div className="intro-text">
                       <div className="Name">Bryan Min</div>
                       <div className="Greeting">
-                        <h6>I’m a student studying Computer Science who loves front-end dev and aesthetic design. Currently, I'm an Undergraduate Research Assistant and Lead Developer in the <a className="t4g" href="https://tech4good.soe.ucsc.edu/#/" target="_blank" rel="noreferrer">Tech4Good Lab</a>.</h6>
+                        <h6>Webdev, HCI research, and minimalism at UCSD. Currently, I'm an Undergraduate Research Assistant and Lead Developer at the <a className="t4g" href="https://tech4good.soe.ucsc.edu/#/" target="_blank" rel="noreferrer">Tech4Good Lab</a>.</h6>
                       </div>
                     </div>
                 </Tween>
