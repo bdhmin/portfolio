2c9794e70facc6c7adb482712af1da0c9e3c6350
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2023-10-19 09:58:35 -0700

    update name

---
+++ src/App/components/tempF/footer.js
@@ -0,0 +1,16 @@
+import './footer.scss'
+
+function Footer() {
+  return (
+    <div className="Footer">
+      {/* <Socials /> */}
+      {/* <span className="msg">정성으로 만든</span> */}
+      <span className="cp">
+        <div>Made by Bryan Min</div>
+        <div>Last Updated:  October 19, 2023</div>
+      </span>
+    </div>
+  )
+}
+
+export default Footer;
