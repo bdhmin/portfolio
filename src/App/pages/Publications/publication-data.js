fa550fccab6e90ade86b4894d90e1d7752f7c673
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2023-10-19 08:53:39 -0700

    Updated publication data

--- src/App/pages/Publications/publication-data.js
+++ src/App/pages/Publications/publication-data.js
@@ -2,19 +2,19 @@ import { projects } from '../../../assets/Projects'
 
 export const publications = [
   {
-    title: 'Demonstration of Masonview',
-    subtitle: 'Content-Driven Viewport Management',
-    authors: ['Bryan Min', 'Matthew T Beaudouin-Lafon', 'Sangho Suh', 'Haijun Xia'],
-    venue: 'UIST 2023 Demos',
+    title: 'ChoiceMates',
+    subtitle: 'Supporting Unfamiliar Online Decision-Making with Multi-Agent Conversational Interactions',
+    authors: ['Jeongeon Park', 'Bryan Min', 'Xiaojuan Ma', 'Juho Kim'],
+    venue: 'arXiv',
     resources: [
-      // {
-      //   type: 'DOI',
-      //   link: 'https://doi.org/10.48550/arXiv.2305.11483',
-      // },
-      // {
-      //   type: 'Paper',
-      //   link: 'https://doi.org/10.48550/arXiv.2305.11483',
-      // },
+      {
+        type: 'DOI',
+        link: 'https://doi.org/10.48550/arXiv.2310.01331',
+      },
+      {
+        type: 'Paper',
+        link: 'https://arxiv.org/pdf/2310.01331.pdf',
+      },
       // {
       //   type: 'Video',
       //   link: 'https://youtu.be/MIfhunAwZew',
@@ -23,6 +23,37 @@ export const publications = [
       //   type: 'Preview',
       //   link: 'https://youtu.be/X3cpRVGuC2w',
       // },
+    ],
+    thumbnail: projects.ChoiceMates,
+    award: '',
+    note: ''
+  },
+  {
+    title: 'Demonstration of Masonview',
+    subtitle: 'Content-Driven Viewport Management',
+    authors: ['Bryan Min', 'Matthew T Beaudouin-Lafon', 'Sangho Suh', 'Haijun Xia'],
+    venue: 'UIST 2023 Demos',
+    resources: [
+      {
+        type: 'DOI',
+        link: 'https://doi.org/10.1145/3586182.3615827',
+      },
+      {
+        type: 'Paper',
+        link: 'https://creativity.ucsd.edu/papers/masonview.pdf',
+      },
+      {
+        type: 'Video',
+        link: 'https://www.youtube.com/watch?v=HwHb02fxe5w&t=7s',
+      },
+      {
+        type: 'Website',
+        link: 'https://creativity.ucsd.edu/masonview',
+      },
+      {
+        type: 'Twitter',
+        link: 'https://x.com/bdhmin/status/1710326548857811044?s=20',
+      },
     ],
     thumbnail: projects.Masonview,
     award: '',
@@ -34,22 +65,30 @@ export const publications = [
     authors: ['Sangho Suh', 'Bryan Min', 'Srishti Palani', 'Haijun Xia'],
     venue: 'UIST 2023',
     resources: [
-      // {
-      //   type: 'DOI',
-      //   link: 'https://doi.org/10.48550/arXiv.2305.11483',
-      // },
-      // {
-      //   type: 'Paper',
-      //   link: 'https://doi.org/10.48550/arXiv.2305.11483',
-      // },
-      // {
-      //   type: 'Video',
-      //   link: 'https://youtu.be/MIfhunAwZew',
-      // },
-      // {
-      //   type: 'Preview',
-      //   link: 'https://youtu.be/X3cpRVGuC2w',
-      // },
+      {
+        type: 'DOI',
+        link: 'https://doi.org/10.48550/arXiv.2305.11483',
+      },
+      {
+        type: 'Paper',
+        link: 'https://arxiv.org/pdf/2305.11483.pdf',
+      },
+      {
+        type: 'Video',
+        link: 'https://www.youtube.com/watch?v=MIfhunAwZew&t=146s',
+      },
+      {
+        type: 'Preview',
+        link: 'https://www.youtube.com/watch?v=X3cpRVGuC2w',
+      },
+      {
+        type: 'Website',
+        link: 'https://creativity.ucsd.edu/ai',
+      },
+      {
+        type: 'Twitter',
+        link: 'https://twitter.com/HaijunXia/status/1646919380704559104?s=20',
+      },
     ],
     thumbnail: projects.Sensecape,
     award: '',
