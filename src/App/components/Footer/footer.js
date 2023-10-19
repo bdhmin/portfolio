5caefe99f33148c5913244f8a90a436fe4379180
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2023-10-19 09:49:31 -0700

    Updated tag

--- src/App/components/Footer/footer.js
+++ src/App/components/Footer/footer.js
@@ -7,7 +7,7 @@ function Footer() {
       {/* <span className="msg">정성으로 만든</span> */}
       <span className="cp">
         <div>Made by Bryan Min</div>
-        <div>Last Updated:  October 9, 2023</div>
+        <div>Last Updated:  October 19, 2023</div>
       </span>
     </div>
   )
